fd6f2814d64cf26de0bf5103214ea35e3cbd2bec
ShadyGitHubb <shaansaxena2009@gmail.com>
2024-08-31 14:04:47 +1200

    Added a jump animation and added animations to Ginger

--- Ginger.gd
+++ Ginger.gd
@@ -8,10 +8,14 @@ func _physics_process(delta):
 	if player_chase:
 		position += (player.position - position) / speed
 
+
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
+	$AnimatedSprite2D.play("Walk")
 
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
+	$AnimatedSprite2D.play("Idle")
+
