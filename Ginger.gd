df6980937c6ac00fb07913ef6c36328eee2bee69
ShadyGitHubb <shaansaxena2009@gmail.com>
2024-09-03 13:17:48 +1200

    Added new ground, added light to Ginger

--- Ginger.gd
+++ Ginger.gd
@@ -1,14 +1,29 @@
 extends CharacterBody2D
 
+@onready var global = get_node("/root/Global")
+
 @export var speed = 35
 var player_chase = false
 var player = null
+var direction = -1
+var gravity = 9.8
+
 
 func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		velocity.y = 0
+		
 	if player_chase:
+		if player.global_position.x < global_position.x:
+			direction = -1
+			$AnimatedSprite2D.scale.x = direction
+		else:
+			direction = 1
+			$AnimatedSprite2D.scale.x = direction
 		position += (player.position - position) / speed
-
-
+		
 
 func _on_detection_area_body_entered(body):
 	player = body
