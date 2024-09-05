6cfbc490f18eec8961caf802588447cfcf1519ff
ShadyGitHubb <shaansaxena2009@gmail.com>
2024-09-05 16:19:49 +1200

    Made Ginger stay on the ground and can jump (kinda)

--- Ginger.gd
+++ Ginger.gd
@@ -7,7 +7,10 @@ var player_chase = false
 var player = null
 var direction = -1
 var gravity = 9.8
+@export var jump_force = -300  # NEW: Added jump force
 
+# NEW: Added ray cast for detecting obstacles
+@onready var ray_cast = $RayCast2D
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -16,14 +19,21 @@ func _physics_process(delta):
 		velocity.y = 0
 		
 	if player_chase:
-		if player.global_position.x < global_position.x:
+		if player.global_position.x < global_position.x - 20:
 			direction = -1
 			$AnimatedSprite2D.scale.x = direction
-		else:
+		elif player.global_position.x > global_position.x + 20:
 			direction = 1
 			$AnimatedSprite2D.scale.x = direction
-		position += (player.position - position) / speed
+		velocity.x = speed * direction
 		
+		# NEW: Check for obstacles and jump
+		if is_on_floor() and ray_cast.is_colliding():
+			var collider = ray_cast.get_collider()
+			if collider is TileMap:
+				velocity.y = jump_force
+		
+	move_and_slide()
 
 func _on_detection_area_body_entered(body):
 	player = body
@@ -33,5 +43,5 @@ func _on_detection_area_body_entered(body):
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
-	$AnimatedSprite2D.play("Idle")
+	
 
